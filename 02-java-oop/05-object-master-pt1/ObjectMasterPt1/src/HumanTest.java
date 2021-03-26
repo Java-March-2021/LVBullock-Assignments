9eb623c30626201b83aadbe403e5a974e7fef9ac
Laura V. Bullock <laura.v.bullock@gmail.com>
2021-03-26 14:41:53 -0400

    OOP - 05 Object Master Part 1

---
+++ 02-java-oop/05-object-master-pt1/ObjectMasterPt1/src/HumanTest.java
@@ -0,0 +1,12 @@
+//Test these methods work using a HumanTest class
+public class HumanTest{
+    public static void main(String[] args) {
+    	
+        System.out.println("\nLaura Bullock - March 2021 Java Cohort - Object Master Part 1 Assignment - March 17, 2021 \n");
+
+        Human human1 = new Human();
+
+        human1.setStrength(3);
+        human1.attack(human1);
+    }
+}
